ede6d71d33cdcce8e0de8afb82bd491deddfc628
RaiYan163 <raiyanashraf20@gmail.com>
2023-12-03 06:34:08 +0600

    test

--- controller/routes.py
+++ controller/routes.py
@@ -108,11 +108,43 @@ def google_login():
 
 
 
+# @app.route('/callback')
+# def callback():
+#     user_info = google_auth.google_callback(google_auth.setup_google_auth())
+#     app.logger.info(f'User info: {user_info}')
+
+#     if user_info:
+#         email = user_info['email']
+#         name = user_info.get('name', 'Unknown')  # Default name to 'Unknown' if not provided
+
+#         # Check if user already exists
+#         user = User.query.filter_by(email=email).first()
+
+#         if user:
+#             # User exists, use the existing user
+#             login_user(user, remember=True)
+#         else:
+#             # Create a new User instance
+#             new_user = User(email=email, firstName=name, password=generate_password_hash('default_password', method='pbkdf2:sha256'))
+#             db.session.add(new_user)
+#             db.session.commit()
+            
+#             # Log in the new user
+#             login_user(new_user, remember=True)
+
+#         # Redirect to the dashboard or another page
+#         return redirect('/dashboard')
+#     else:
+#         # Handle login failure
+#         app.logger.error('Authentication failed. No user info received.')
+#         flash('Authentication failed.', category='error')
+#         return redirect(url_for('login'))
+
+
 @app.route('/callback')
 def callback():
     google_auth.google_callback(google_auth.setup_google_auth())
     return redirect("/dashboard")
-
 
 
 @app.route('/summary')
@@ -124,7 +156,19 @@ def summary():
 @app.route('/dashboard')
 @optional_login_required
 def dashboard():
-    return render_template('dashboard.html', user=current_user)
+    if current_user.is_authenticated and hasattr(current_user, 'userID') and current_user.userID is not None:
+        summaries_count = Summary.query.filter_by(userID=current_user.userID).count()
+        links_count = SavedLink.query.filter_by(userID=current_user.userID).count()
+    else:
+        # Set counts to 0 if userID is nil or user is not authenticated
+        summaries_count = 0
+        links_count = 0
+
+    return render_template('dashboard.html', 
+                           user=current_user, 
+                           summaries_count=summaries_count, 
+                           links_count=links_count)
+
 
 @app.route('/show_items')
 @optional_login_required
@@ -156,27 +200,37 @@ def settings():
     return render_template('settings.html')
 
 
+# @app.route('/showSummary')
+# @optional_login_required
+# def showSummary():
+#     # Define the number of summaries per page
+#     per_page = 5
+#     page = request.args.get('page', 1, type=int)  # Get the page number from the query parameter
+
+#     # Fetch the total number of summaries for pagination calculation
+#     total_summaries = Summary.query.filter_by(userID=current_user.userID).count()
+
+#     # Calculate the total number of pages needed
+#     total_pages = ceil(total_summaries / per_page)
+
+#     # Fetch summaries for the current page, sorted by summaryID in descending order
+#     summaries = Summary.query.filter_by(userID=current_user.userID)\
+#                              .order_by(Summary.summaryID.desc())\
+#                              .offset((page - 1) * per_page)\
+#                              .limit(per_page)\
+#                              .all()
+
+#     return render_template('showSummary.html', summaries=summaries, current_page=page, total_pages=total_pages)
+
 @app.route('/showSummary')
 @optional_login_required
 def showSummary():
-    # Define the number of summaries per page
-    per_page = 5
-    page = request.args.get('page', 1, type=int)  # Get the page number from the query parameter
-
-    # Fetch the total number of summaries for pagination calculation
-    total_summaries = Summary.query.filter_by(userID=current_user.userID).count()
-
-    # Calculate the total number of pages needed
-    total_pages = ceil(total_summaries / per_page)
-
-    # Fetch summaries for the current page, sorted by summaryID in descending order
-    summaries = Summary.query.filter_by(userID=current_user.userID)\
-                             .order_by(Summary.summaryID.desc())\
-                             .offset((page - 1) * per_page)\
-                             .limit(per_page)\
+    # Fetch summaries for the user, sorted by summaryID in descending order
+    summaries = Summary.query.filter_by(userID=current_user.userID) \
+                             .order_by(Summary.summaryID.desc()) \
                              .all()
 
-    return render_template('showSummary.html', summaries=summaries, current_page=page, total_pages=total_pages)
+    return render_template('showSummary.html', summaries=summaries)
 
 
 # @app.route('/showSummary/<int:summary_id>')
@@ -204,6 +258,89 @@ def showLinks():
         links = SavedLink.query.filter_by(userID=current_user.userID, linkCategory=selected_category).all()
 
     return render_template('showLinks.html', links=links, selected_category=selected_category)
+
+@app.route('/linkEdit', methods=['GET', 'POST'])
+@optional_login_required
+def link_edit():
+    if request.method == 'POST':
+        link_title = request.form.get('linkTitle')
+        link_url = request.form.get('linkURL')
+        link_description = request.form.get('description')
+        link_category = request.form.get('category')
+
+        # Create a new SavedLink instance
+        new_link = SavedLink(
+            userID=current_user.userID,
+            linkTitle=link_title,
+            linkURL=link_url,
+            linkDescription=link_description,
+            linkCategory=link_category
+        )
+
+        # Add the new link to the session and commit to the database
+        db.session.add(new_link)
+        db.session.commit()
+
+        # Flash a success message
+        flash('Link saved successfully!', category='success')
+        
+        # Redirect to the dashboard
+        return redirect(url_for('dashboard'))
+
+    # For GET request, check for the YouTube URL passed as a query parameter
+    yt_url = request.args.get('yt_url', '')
+
+    # Render the linkEdit template, passing the YouTube URL if available
+    return render_template('linkEdit.html', yt_url=yt_url)
+
+@app.route('/summaryEdit', methods=['GET', 'POST'])
+@optional_login_required
+def summary_edit():
+    if request.method == 'POST':
+        summary_title = request.form.get('summaryTitle')
+        summary_link = request.form.get('summaryURL')
+        summary_body = request.form.get('body')
+        rating = request.form.get('rating')  # Retrieve the rating from the form
+
+        # Convert rating to integer, default to None if conversion fails
+        try:
+            rating = int(rating)
+        except ValueError:
+            rating = None
+
+        # Create a new Summary instance
+        new_summary = Summary(
+            userID=current_user.userID,
+            summaryTitle=summary_title,
+            summaryLink=summary_link,
+            summaryBody=summary_body,
+            rating=rating  # Save the integer rating
+        )
+
+        # Add the new summary to the session and commit to the database
+        db.session.add(new_summary)
+        db.session.commit()
+
+        # Flash a success message
+        flash('Summary saved successfully!', category='success')
+        
+        # Redirect to a relevant page, such as the dashboard or summary list
+        return redirect(url_for('dashboard'))
+    
+    else:
+            # For a GET request, retrieve the query parameters
+            summary_link = request.args.get('summaryURL', '')
+            summary_body = request.args.get('body', '')
+
+            # Render the template with the pre-filled data
+            return render_template('summaryEdit.html', 
+                               summary_link=summary_link, 
+                               summary_body=summary_body)
+
+   
+    
+
+
 
 
 
